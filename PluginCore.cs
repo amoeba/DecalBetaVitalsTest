28c2fb05ef6c55423fc933a26a50a6f92a386232
Bryce Mecum <petridish@gmail.com>
2023-01-18 16:19:20 -0900

    Add project files.

---
+++ PluginCore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Decal.Adapter;
+using Decal.Adapter.Wrappers;
+using Decal.Interop.Core;
+
+namespace TestPlugin
+{
+    public class PluginCore : PluginBase
+    {
+        protected override void Startup()
+        {
+            Core.CharacterFilter.LoginComplete += CharacterFilter_LoginComplete;
+        }
+
+        protected override void Shutdown()
+        {
+            Core.CharacterFilter.LoginComplete -= CharacterFilter_LoginComplete;
+        }
+
+        private void CharacterFilter_LoginComplete(object sender, EventArgs e)
+        {
+            ///
+            Log("Iterating over vitals...");
+            foreach (var vital in Core.CharacterFilter.Vitals)
+            {
+                Log($"Vital: {vital.Name}; Base: {vital.Base}");
+            }
+            Log("...Done.");
+
+            ///
+
+            Log("Printing Vital Name by Index...");
+            Log(Core.CharacterFilter.Vitals[CharFilterVitalType.Health].Name);
+            Log(Core.CharacterFilter.Vitals[CharFilterVitalType.Stamina].Name);
+            Log(Core.CharacterFilter.Vitals[CharFilterVitalType.Mana].Name);
+            Log("...Done.");
+
+            ///
+
+            Decal.Interop.Filters.SkillInfo health = Core.CharacterFilter.Underlying.Vital[Decal.Interop.Filters.eVitalID.eHealth];
+            Decal.Interop.Filters.SkillInfo stamina = Core.CharacterFilter.Underlying.Vital[Decal.Interop.Filters.eVitalID.eStamina];
+            Decal.Interop.Filters.SkillInfo mana = Core.CharacterFilter.Underlying.Vital[Decal.Interop.Filters.eVitalID.eMana];
+
+            Log("Iterating over vitals via Underlying...");
+            Log($"Vital: {health.Name}; Base: {health.Base}");
+            Log($"Vital: {stamina.Name}; Base: {stamina.Base}");
+            Log($"Vital: {mana.Name}; Base: {mana.Base}");
+            Log("...Done.");
+        }
+
+        private void Log(string message)
+        {
+            Host.Actions.AddChatText(message, 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+﻿Iterating over vitals...
+Vital: ⚈ཾ; Base: 15
+Vital: Stamina; Base: 30
+Vital: Mana; Base: 10
+...Done.
+
+Iterating over vitals...
+Vital: ⾨ཬ; Base: 15
+Vital: Stamina; Base: 30
+Vital: Mana; Base: 10
+...Done.
+
+Iterating over vitals...
+Vital: 仰ጋ; Base: 15
+Vital: Stamina; Base: 30
+Vital: Mana; Base: 10
+...Done.
